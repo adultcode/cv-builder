472b675765f99e63538fdda3741f69cb9604442a
hharddy <hharddy@yahoo.com>
2023-12-25 20:37:13 +0330

    bg color

--- lib/cv.dart
+++ lib/cv.dart
@@ -4,6 +4,7 @@
 import 'dart:math';
 import 'dart:typed_data';
 
+import 'package:cv_builder/util/color.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/services.dart';
 import 'package:pdf/pdf.dart';
@@ -20,11 +21,11 @@ Future<Uint8List> generateResume(PdfPageFormat format,BuildContext buildContext)
   // );
 
   final pageTheme = await _myPageTheme(format);
-  format = format.copyWith(
-      marginBottom: 0.0,
-      marginLeft: 1.0*PdfPageFormat.inch ,
-      marginRight: 2.0*PdfPageFormat.inch,
-      marginTop: 0.0 );
+  // format = format.copyWith(
+  //     marginBottom: 0.0,
+  //     marginLeft: 1.0*PdfPageFormat.inch ,
+  //     marginRight: 2.0*PdfPageFormat.inch,
+  //     marginTop: 0.0 );
   doc.addPage(
     // pw.Text('حسام رسولیانث',
     //     //  textScaleFactor: 1,
@@ -32,35 +33,60 @@ Future<Uint8List> generateResume(PdfPageFormat format,BuildContext buildContext)
     //     textDirection: pw.TextDirection.rtl
     // )
     pw.MultiPage(
+
      pageTheme: pageTheme,
-     // pageFormat: format,
+     // pageFormat: format,,
       build: (pw.Context context) => [
-        pw.Container(
-          alignment: pw.Alignment.topRight,
-          child: pw.Row(
-            mainAxisAlignment: pw.MainAxisAlignment.end,
-            children: [
-              pw.Expanded(
-                flex: 10,
-                child:  pw.Container(
-               //   width: 10*PdfPageFormat.cm,
-                  height: 100,
+        pw.Partition(
+          child: pw.Container(
+            // color: black_title1,
+            // height: MediaQuery.of(buildContext).size.height,
+            alignment: pw.Alignment.topRight,
+            child: pw.Row(
+                mainAxisAlignment: pw.MainAxisAlignment.end,
+                children: [
+                  /*
+              left side
+               */
+                  pw.Expanded(
+                    flex: 10,
+                    child:  pw.Container(
+                      //   width: 10*PdfPageFormat.cm,
+                        height: 100,
 
-                  color: PdfColor.fromInt(0xff9ce5d0)
-              ),
-              ),
-              pw.Expanded(
-                  flex: 18,
+                        color: PdfColor.fromInt(0xff9ce5d0)
+                    ),
+                  ),
 
-                  child: pw.Container(
-                  // width: 3.0*PdfPageFormat.cm,
-                    height: 100,
-                    color: PdfColor.fromInt(0xff9005d0)
-                )
-              )
-            ]
+                  /*
+              right side
+               */
+                  pw.Expanded(
+                      flex: 18,
+
+                      child: pw.Container(
+                        // width: 3.0*PdfPageFormat.cm,
+                          height: 100,
+                          color: PdfColor.fromInt(0xff9005d0),
+                          child: pw.Column(
+                              children:[
+                                pw.Container(
+                                  width: 60,
+                                  height: 60,
+                                  decoration: pw.BoxDecoration(
+                                      color: black_title1,
+                                      shape: pw.BoxShape.circle
+                                  ),
+                                  // color:  PdfColor.fromInt(0xff9ce5d0)
+                                )
+                              ]
+                          )
+                      )
+                  )
+                ]
+            ),
           ),
-        ),
+        )
       ],
     ),
   );
@@ -75,36 +101,46 @@ Future<pw.PageTheme> _myPageTheme(PdfPageFormat format) async {
 
   format = format.copyWith(
       marginBottom: 0.0,
-      marginLeft: 1.0*PdfPageFormat.cm ,
-      marginRight: 1.0*PdfPageFormat.cm,
+      marginLeft: 0.0*PdfPageFormat.cm ,
+      marginRight: 0.0*PdfPageFormat.cm,
       marginTop: 0.0 );
 
 
   return pw.PageTheme(
     pageFormat: format,
+
     theme: pw.ThemeData.withFont(
+
       base: font,
       bold: await PdfGoogleFonts.openSansBold(),
       icons: await PdfGoogleFonts.materialIcons(),
     ),
+
     buildBackground: (pw.Context context) {
       return pw.FullPage(
+
         ignoreMargins: true,
-        child: pw.Stack(
-          children: [
-            // pw.Positioned(
-            //   child: pw.SvgImage(svg: bgShape),
-            //   left: 0,
-            //   top: 0,
-            // ),
-            // pw.Positioned(
-            //   child: pw.Transform.rotate(
-            //       angle: pi, child: pw.SvgImage(svg: bgShape)),
-            //   right: 0,
-            //   bottom: 0,
-            // ),
-          ],
-        ),
+        child: pw.Expanded(
+          child: pw.Container(
+            color: bg_color1
+          )
+        )
+        // child: pw.Stack(
+        //   children: [
+        //
+        //     // pw.Positioned(
+        //     //   child: pw.SvgImage(svg: bgShape),
+        //     //   left: 0,
+        //     //   top: 0,
+        //     // ),
+        //     // pw.Positioned(
+        //     //   child: pw.Transform.rotate(
+        //     //       angle: pi, child: pw.SvgImage(svg: bgShape)),
+        //     //   right: 0,
+        //     //   bottom: 0,
+        //     // ),
+        //   ],
+        // ),
       );
     },
   );
---
+++ lib/util/color.dart
@@ -0,0 +1,4 @@
+ import 'package:pdf/pdf.dart';
+
+var bg_color1 = PdfColor.fromHex('#dbdbdb');
+var black_title1 = PdfColor.fromHex('#343434');
