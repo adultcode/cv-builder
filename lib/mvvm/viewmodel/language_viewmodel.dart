55c595d5fbbdd4c1d68a744b831e6a1a80538d66
hharddy <hharddy@yahoo.com>
2024-01-27 19:52:10 +0330

    language viewmodel

---
+++ lib/mvvm/viewmodel/language_viewmodel.dart
@@ -0,0 +1,174 @@
+import 'package:cv_builder/mvvm/model/entity/education_model/education_list.dart';
+import 'package:cv_builder/mvvm/model/entity/education_model/education_model.dart';
+import 'package:cv_builder/mvvm/model/entity/language/language_list.dart';
+import 'package:cv_builder/mvvm/model/entity/language/language_model.dart';
+import 'package:cv_builder/mvvm/model/entity/skill_model/skill_model.dart';
+import 'package:cv_builder/mvvm/repository/education_repo.dart';
+import 'package:cv_builder/mvvm/repository/language_repo.dart';
+import 'package:cv_builder/mvvm/repository/skill_repo.dart';
+import 'package:flutter/cupertino.dart';
+
+import '../../widget/custom_widgets/panel/dashboard/items/education_item.dart';
+import '../../widget/custom_widgets/panel/dashboard/items/language_item.dart';
+import '../../widget/custom_widgets/panel/dashboard/items/skill_item.dart';
+
+class LanguageVM extends ChangeNotifier{
+
+
+  // SocialModel? socialModel;
+  LanguageList? languageList;
+  List<Widget>? lang_items=[];
+  LanguageModel? selected_lang;
+  bool isForEdit = false;
+  late LanguageRepository  languageRepository;
+
+  LanguageVM(){
+    languageRepository = LanguageRepository();
+    languageList = LanguageList();
+    //  languageList?.languageList = [];
+
+  }
+
+  void SelectSkillModel(LanguageModel language_model){
+
+    print("Selected work model clicked");
+    selected_lang = language_model;
+    notifyListeners();
+    isForEdit = true;
+  }
+
+
+// delete workmodel from list
+  void DeleteSkill(LanguageModel language_model){
+
+    print("Selected work model e");
+    selected_lang = language_model;
+    languageList?.lang_list?.remove(language_model);
+
+    // proccess list again
+    lang_items?.clear();
+    lang_items = languageList?.lang_list?.map((e) => LanguageItem(languageModel: e!)).toList();
+
+    isForEdit = false;
+    selected_lang = null;
+    notifyListeners();
+
+
+  }
+
+
+  /*
+  add new workmodel in list
+   */
+  void AddSkill({required LanguageModel language_model})async{
+
+    print('length: ${languageList?.lang_list}');
+    if(languageList?.lang_list==null || languageList?.lang_list?.isEmpty==true){
+      print("lang list is empty");
+      language_model.id = 1;
+      print("lang $language_model");
+
+      languageList?.lang_list=[language_model];
+      // languageList?.languageList?.add(educationModel);
+      print('length2: ${languageList?.lang_list}');
+
+
+    }
+    else{
+      var last_id = (languageList?.lang_list?.last!.id!)! ;
+      last_id= last_id + 1;
+      // check isForEdit is true or not
+      // if true, it's edit not new item
+      if(isForEdit==true){
+        //find index
+
+        int? _index = languageList?.lang_list?.indexWhere((item) => item?.id == selected_lang?.id);
+
+        print("index for edit is $_index");
+        if(_index!=null) {
+          language_model.id = selected_lang?.id;
+          languageList?.lang_list?[_index] = language_model;
+
+        }
+
+      }else{
+        language_model.id =last_id;
+        languageList?.lang_list?.add(language_model);
+
+      }
+
+    }
+
+    // worklList?.workModels?.add(work);
+
+    lang_items?.clear();
+    lang_items = languageList?.lang_list?.map((e) => LanguageItem(languageModel: e!)).toList();
+
+    print("Data added");
+    isForEdit = false;
+    selected_lang = null;
+    notifyListeners();
+
+  }
+
+  /*
+  save list of work models
+   */
+  void SavelanguageList({required LanguageList languageList})async{
+    var result;
+    if(languageList.lang_list?.isEmpty==true){
+      print("List is empty delete all works");
+      result = await languageRepository.ClearLanguageList();
+
+    }else{
+      this.languageList  = languageList;
+      print("Saving data:");
+      result = await languageRepository.SaveLanguageDataList(languageList: languageList);
+
+
+    }
+
+    if(result==true){
+      lang_items?.clear();
+      lang_items = languageList.lang_list?.map((e) => LanguageItem(languageModel: e!)).toList();
+
+      print("Data saved");
+      notifyListeners();
+
+    }else{
+      // show snackbar error
+      print("this is an error");
+    }
+  }
+
+  /*
+  get list of work models save in shared prefences
+   */
+  void GetlanguageListData()async{
+    try{
+      var result = await languageRepository.GetLanguageListData();
+
+      languageList = result;
+      lang_items?.clear();
+//  lang_items = worklList?.workModels?.map((e) => WorkItem(e!)).toList();
+      if(result!=null){
+        print("Key exist");
+        lang_items = languageList?.lang_list?.map((e) => LanguageItem(languageModel: e!)).toList();
+      }else{
+        print("Data is null key nit exist");
+        lang_items = null;
+      }
+      //print("Get size: ${result.workModels}");
+
+
+    }catch(e){
+      lang_items = null;
+
+      print("Get Data:  ${e.toString()}");
+    }
+
+    notifyListeners();
+    isForEdit = false;
+  }
+
+}
---
+++ lib/widget/custom_widgets/panel/dashboard/items/language_item.dart
@@ -0,0 +1,14 @@
+import 'package:cv_builder/mvvm/model/entity/language/language_model.dart';
+import 'package:flutter/material.dart';
+
+class LanguageItem extends StatelessWidget {
+
+  LanguageModel languageModel;
+
+  LanguageItem({required this.languageModel});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
